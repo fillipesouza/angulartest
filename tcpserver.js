82ebc325bc608070db75508607646e517c91bd73
fillipesouza <ss.fillipe@gmail.com>
2019-08-19 21:58:39 -0300

    Apenas teste
    Modifiquei o código adicionando uma linha "Aloha"

--- tcpserver.js
+++ tcpserver.js
@@ -3,6 +3,7 @@ var net=require('net');
 var server = net.createServer(function (socket){
 	console.log("Connection from " + socket.remoteAddress);
 	socket.end("Hello World");
+	console.log("Aloha");
 });
 
 server.listen(7000, "localhost");
